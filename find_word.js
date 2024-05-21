b70ae01875055ab1576fa34c4b84828883c40ac8
devjmv <javier161890@gmail.com>
2024-05-21 09:57:27 +0200

    updated find_word.js

--- find_word.js
+++ find_word.js
@@ -1,9 +1,24 @@
-var text = "holaeceoeoEnglIshsss";
-text = text.toUpperCase();
+var text;
 
-if (text.includes('ENGLISH')) {
-    console.log(true);
-}
-else {
-    console.log(false);
-}
+process.stdout.write("Entra el texto: ");
+
+process.stdin.on('data', function (data) {
+    text = data.toString();
+    text = text.toUpperCase();
+    if (text.includes('ENGLISH')) {
+        console.log(true);
+    }
+    else {
+        console.log(false);
+    }
+    process.exit();
+});
+
+//text = text.toUpperCase();
+//
+//if (text.includes('ENGLISH')) {
+//    console.log(true);
+//}
+//else {
+//    console.log(false);
+//}
