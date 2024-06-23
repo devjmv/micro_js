0f6cfdc6f629b7b3cbccb765cc8e7972a7bdf87d
devjmv <javier161890@gmail.com>
2024-06-23 22:13:34 +0200

    add reverse words js

---
+++ src/ReverseWords/reverseWords.js
@@ -0,0 +1,3 @@
+const cadenaADividir = 'Hello World'
+const arrayDeCadenas = cadenaADividir.split(' ').reverse().join(' ')
+console.log(arrayDeCadenas);
--- src/async/app.js
+++ src/async/app.js
@@ -1,16 +1,13 @@
-let dataJason = '{"results": [{"title": "Estudiar Js","priority": "alta","isDone": false},{"title": "Estudiar CSS","priority": "alta","isDone": true},{"title": "Estudiar OOP","priority": "media","isDone": false},{"title": "Estudiar IA","priority": "baja","isDone": false}]}'
+let dir = './data.json'
 
-let results = JSON.parse(dataJason);
-//let results = {
-//    title: String,
-//    priority: String,
-//    isDone: Boolean,
-//};
+async function datos(){
+    const response = await fetch(dir)
+    const dat = await response.json()
+    //console.log(dat.results);
 
-//function results(title, priority, isDone) {
-//    this.title = title;
-//    this.priority = priority;
-//    this.isDone = isDone;
-//}
+    dat.results.forEach(element => {
+        console.log(element[0]);
+    });
+}
 
-console.log(results)
+datos();
